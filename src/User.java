40667a9760489ba303cc31d1291afed4c17213f5
RossNeyman <ross.neyman@gmail.com>
2025-02-27 15:36:40 -0500

    First commit

---
+++ src/User.java
@@ -0,0 +1,28 @@
+public class User {
+    private final int userID;
+    private String userName;
+    private String password;
+    private String userEmail;
+    private int userRole;
+
+    public User(String userName, String password, String userEmail, int userRole) {
+        this.userID = 1;
+        this.userName = userName;
+        this.password = password;
+        this.userEmail = userEmail;
+        this.userRole = userRole;
+    }
+    public int getUserID() {return userID;}
+    public String getUserName() {return userName;}
+    public String getPassword() {return password;}
+    public String getUserEmail() {return userEmail;}
+    public int getUserRole() {return userRole;}
+    public void setUserRole(int userRole) {this.userRole = userRole;}
+    public void setUserName(String userName) {this.userName = userName;}
+    public void setPassword(String password) {this.password = password;}
+    public void setUserEmail(String userEmail) {this.userEmail = userEmail;}
+
+    public void generateRandomId() {
+        //TODO add random ID generator with checking for existing ID
+    }
+}
